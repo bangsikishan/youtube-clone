c909ae603c6ea515c11b8b2b50c2a1fdb876e57b
Kishan <bang.siki2009@gmail.com>
2023-02-12 21:01:30 +0545

    Added card to show videos

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,9 +1,17 @@
+import { Stack, Box } from "@mui/system";
+
+import { VideoCard, ChannelCard } from './';
+
 const Videos = ({ videos }) => {
-    console.log(videos);
     return (
-        <div>
-            videos
-        </div>
+        <Stack direction="row" flexWrap="wrap" justifyContent="center" gap={2}>
+            {videos.map((item, idx) => (
+                <Box key={idx}>
+                    {item.id.videoId && <VideoCard video={item} />}
+                    {item.id.channelId && <ChannelCard channelDetail={item} />}
+                </Box>
+            ))}
+        </Stack>
     );
 }
  
