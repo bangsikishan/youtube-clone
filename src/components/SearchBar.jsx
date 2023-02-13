76ffb351fc049c5bb0a364054d937d5fbbfd0a88
Kishan <bang.siki2009@gmail.com>
2023-02-13 12:08:37 +0545

    Add a package and elements to render videos

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,9 +4,23 @@ import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const navigate = useNavigate();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(searchTerm) {
+            navigate(`/search/${searchTerm}`);
+
+            setSearchTerm('');
+        }
+    }
+
     return (
-        <Paper component="form" onSubmit={() => {}} sx={{ borderRadius: 20, border: '1px solid #e3e3e3', pl: 2, boxShadow: 'none', mr: {sm:5} }}>
-            <input className='search-bar' type="text" value="" onChange={() => {}} placeholder='Search...' />
+        <Paper component="form" onSubmit={handleSubmit} sx={{ borderRadius: 20, border: '1px solid #e3e3e3', pl: 2, boxShadow: 'none', mr: {sm:5} }}>
+            <input className='search-bar' type="text" value={searchTerm} onChange={(e) => {setSearchTerm(e.target.value)}} placeholder='Search...' />
             <IconButton type="submit" sx={{ p: '10px', color: 'red' }}>
                 <Search />
             </IconButton>
